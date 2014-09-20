c17e452e42f171ad14e623b3fc0be11a45e4ae92
ande3577 <ande3577@gmail.com>
2014-09-20 07:19:37 -0500

    Add permissions

--- init.rb
+++ init.rb
@@ -5,4 +5,11 @@ Redmine::Plugin.register :redmine_daily_scrum do
   version '0.0.1'
   url 'http://github.com/ande3577/redmine_daily_scrum'
   author_url 'http://github.com/ande3577'
+
+  project_module :daily_scrum do
+    permission :view_daily_scrum, :daily_scrum => [:index, :show]
+    permission :edit_daily_scrum, :daily_scrum => [:create, :new, :edit, :update]
+    permission :view_own_daily_scrum, :daily_scrum => [:index, :show]
+    permission :edit_own_daily_scrum, :daily_scrum => [:create, :new, :edit, :update]
+  end
 end
